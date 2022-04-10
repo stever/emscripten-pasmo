375111774bc56fd824f552f4166399160d8bbf27
Steven Robertson <steverobertson@gmail.com>
2022-04-10 19:22:56 +0100

    Implemented a bin2tap function

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const Module = require('./dist/pasmo.js')
+const Module = require('./dist/pasmo.js');
 
-module.exports = (input, print, printErr) => {
-    return new Promise((resolve, reject) => {
+// TODO: Command error handling!
+
+export default function compile(asmInput) {
+    return new Promise((resolve/*, reject*/) => {
         Module({
-            'print': print,
-            'printErr': printErr,
-            'input': input,
+            'print': console.log,
+            'printErr': console.error,
+            'asmInput': asmInput,
+            'resolve': resolve,
+            'arguments': ['--tapbas', 'input.asm', 'output.tap']
+        });
+    });
+}
+
+export function bin2tap(binInput) {
+    const asmInput = `ORG 0x8000
+INCBIN input.bin
+END 0x8000`;
+    return new Promise((resolve/*, reject*/) => {
+        Module({
+            'asmInput': asmInput,
+            'binInput': binInput,
             'resolve': resolve,
             'arguments': ['--tapbas', 'input.asm', 'output.tap']
         });
--- src/pre.js
+++ src/pre.js
@@ -1,4 +1,11 @@
 Module['preRun'] = [];
 Module['preRun'].push(function () {
-    FS.writeFile('input.asm', Module['input']);
+
+    // NOTE: Always using assembly input file.
+    FS.writeFile('input.asm', Module['asmInput']);
+
+    // NOTE: The bin2tap function will set this property.
+    if (Module.hasOwnProperty('binInput')) {
+        FS.writeFile('input.bin', Module['binInput']);
+    }
 });
